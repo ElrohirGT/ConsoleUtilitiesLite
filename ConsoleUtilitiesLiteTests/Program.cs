30fceb2c0a532e194a1393b68ce47b6a80c48932
ElrohirGT <elrohirgt@gmail.com>
2021-09-23 09:08:40 -0600

    Added a tests project

---
+++ ConsoleUtilitiesLiteTests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static ConsoleUtilitiesLite.ConsoleUtilities;
+
+namespace ConsoleUtilitiesLiteTests
+{
+    internal class Program
+    {
+        private static string[] title = new string[]
+        {
+            "────────────────────────────────────────────────────────────────────────",
+            "─██████████████─██████████─██████████████─██████─────────██████████████─",
+            "─██░░░░░░░░░░██─██░░░░░░██─██░░░░░░░░░░██─██░░██─────────██░░░░░░░░░░██─",
+            "─██████░░██████─████░░████─██████░░██████─██░░██─────────██░░██████████─",
+            "─────██░░██───────██░░██───────██░░██─────██░░██─────────██░░██─────────",
+            "─────██░░██───────██░░██───────██░░██─────██░░██─────────██░░██████████─",
+            "─────██░░██───────██░░██───────██░░██─────██░░██─────────██░░░░░░░░░░██─",
+            "─────██░░██───────██░░██───────██░░██─────██░░██─────────██░░██████████─",
+            "─────██░░██───────██░░██───────██░░██─────██░░██─────────██░░██─────────",
+            "─────██░░██─────████░░████─────██░░██─────██░░██████████─██░░██████████─",
+            "─────██░░██─────██░░░░░░██─────██░░██─────██░░░░░░░░░░██─██░░░░░░░░░░██─",
+            "─────██████─────██████████─────██████─────██████████████─██████████████─",
+            "────────────────────────────────────────────────────────────────────────"
+        };
+
+        private static void Main(string[] args)
+        {
+            TitleTest();
+            DeleteTest();
+            ReallyLargeLogDelete();
+        }
+
+        private static void ReallyLargeLogDelete()
+        {
+            int length = LogInfoMessage(new string('-', Console.BufferWidth * 150));
+            ClearPreviousLog(length);
+            LogInfoMessage("Only this should be on screen.");
+        }
+
+        private static void DeleteTest()
+        {
+            int length = LogInfoMessage("This will be deleted!");
+            ClearPreviousLog(length);
+            LogInfoMessage("This will not be deleted!");
+        }
+
+        private static void TitleTest()
+        {
+            ShowTitle(title);
+        }
+    }
+}
